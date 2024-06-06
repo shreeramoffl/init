0740b6b65e8496e713dd13e11e5f64fd84213571
ShreeRam <shreeram.techie@gmail.com>
2024-06-06 22:46:42 +0530

    Welcome JavaScript!

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+var a, b;
+a=10
+b=15;
+console.log(a+b);
+
+console.log("iphone15")
+var price = 10
+var product = 'iphone15'
+var tax =10
+console.log(product)
+var total=price+tax
+console.log(total)
+
+var a=10
+a++
+console.log(a)
+
